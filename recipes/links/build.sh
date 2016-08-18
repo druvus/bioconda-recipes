1dd1e5a430519fb634a83cb1537f3f99c5e81605
Andreas Sjödin <andreas.sjodin@gmail.com>
2016-08-18 18:11:43 +0200

    Bump links to 1.8.3

--- recipes/links/build.sh
+++ recipes/links/build.sh
@@ -1,4 +1,10 @@
 mkdir -p $PREFIX/bin
+mkdir -p $PREFIX/opt/links
 
-cp  LINKS $PREFIX/bin
-chmod +x $PREFIX/bin/LINKS
+cd lib/bloomfilter/swig/
+PERL5DIR=`(perl -e 'use Config; print $Config{archlibexp}, "\n";') 2>/dev/null`
+swig -Wall -c++ -perl5 BloomFilter.i
+g++ -c BloomFilter_wrap.cxx -I$PERL5DIR/CORE -fPIC -Dbool=char -O3
+g++ -Wall -shared BloomFilter_wrap.o -o BloomFilter.so -O3
+
+ln -s  $PREFIX/opt/links/LINKS $PREFIX/bin/LINKS
