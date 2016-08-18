6b3a17fa64e0b9aecf7c74ac840e2f7ef5b59e56
Andreas Sjödin <andreas.sjodin@gmail.com>
2016-08-18 18:21:31 +0200

    Copy LINKS binary

--- recipes/links/build.sh
+++ recipes/links/build.sh
@@ -6,5 +6,7 @@ PERL5DIR=`(perl -e 'use Config; print $Config{archlibexp}, "\n";') 2>/dev/null`
 swig -Wall -c++ -perl5 BloomFilter.i
 g++ -c BloomFilter_wrap.cxx -I$PERL5DIR/CORE -fPIC -Dbool=char -O3
 g++ -Wall -shared BloomFilter_wrap.o -o BloomFilter.so -O3
+cp LINKS $PREFIX/opt/links/LINKS
+cp -R lib $PREFIX/opt/links
 
 ln -s  $PREFIX/opt/links/LINKS $PREFIX/bin/LINKS
