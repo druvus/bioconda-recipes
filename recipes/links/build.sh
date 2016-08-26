af3068e6f2df0c81b70ec198d07cfaa6658f9fba
Andreas Sjödin <andreas.sjodin@gmail.com>
2016-08-26 14:10:36 +0000

    Clean up links recipe

--- recipes/links/build.sh
+++ recipes/links/build.sh
@@ -1,12 +1,3 @@
 mkdir -p $PREFIX/bin
-mkdir -p $PREFIX/opt/links
 
-cd lib/bloomfilter/swig/
-PERL5DIR=`(perl -e 'use Config; print $Config{archlibexp}, "\n";') 2>/dev/null`
-swig -Wall -c++ -perl5 BloomFilter.i
-g++ -c BloomFilter_wrap.cxx -I$PERL5DIR/CORE -fPIC -Dbool=char -O3
-g++ -Wall -shared BloomFilter_wrap.o -o BloomFilter.so -O3
-cp LINKS $PREFIX/opt/links/LINKS
-cp -R lib $PREFIX/opt/links
-
-ln -s  $PREFIX/opt/links/LINKS $PREFIX/bin/LINKS
+cp LINKS $PREFIX/bin
