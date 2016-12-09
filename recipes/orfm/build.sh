03adf834315cdce5954401a6d7d4a25fd615fdb3
Andreas Sjödin <andreas.sjodin@gmail.com>
2016-12-09 21:33:25 +0100

    Add orfm

---
+++ recipes/orfm/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd $SRC_DIR/ext
+wget -O seqtk.tar.gz https://github.com/lh3/seqtk/archive/d3b53c9.tar.gz
+tar -xvf seqtk.tar.gz -C seqtk --strip-components 1
+cd ..
+
+mkdir -p $PREFIX/bin
+./autogen.sh
+./configure 
+make
+mv orfm  $PREFIX/bin
