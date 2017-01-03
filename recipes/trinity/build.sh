44f0d7a8139453f4d6125f5cfed3f46072328520
Andreas Sjödin <andreas.sjodin@gmail.com>
2017-01-03 10:46:14 +0100

    Fix trinity build

--- recipes/trinity/build.sh
+++ recipes/trinity/build.sh
@@ -2,6 +2,18 @@
 
 set -x -e
 
+# fix automake
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/aclocal
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/automake
+
+# fix autoconf
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/autom4te
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/autoheader
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/autoreconf
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/ifnames
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/autoscan
+sed -i.bak '1 s|^.*$|#!/usr/bin/env perl|g' $PREFIX/bin/autoupdate
+
 export CC=${PREFIX}/bin/gcc
 export CXX=${PREFIX}/bin/g++
 export INCLUDE_PATH="${PREFIX}/include"
@@ -22,6 +34,7 @@ make inchworm_target
 make chrysalis_target
 
 cd $SRC_DIR/trinity-plugins/
+autoreconf
 make scaffold_iworm_contigs_target
 cd $SRC_DIR
 
