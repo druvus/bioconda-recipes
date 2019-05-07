3c6bd7eee5a0a96948b2b9b882a7674a2b3bd828
Andreas Sjödin <andreas.sjodin@gmail.com>
2019-05-07 22:19:18 +0200

    Rebuild perl-heap-simple-xs

--- recipes/perl-heap-simple-xs/build.sh
+++ recipes/perl-heap-simple-xs/build.sh
@@ -3,15 +3,15 @@
 # If it has Build.PL use that, otherwise use Makefile.PL
 if [ -f Build.PL ]; then
     perl Build.PL
-    ./Build
-    #./Build test # expected to fail as we don't had perl-heap-simple as dep to avoid cyclic dependency
+    perl ./Build
+    perl ./Build test
     # Make sure this goes in site
-    ./Build install --installdirs site
+    perl ./Build install --installdirs site
 elif [ -f Makefile.PL ]; then
     # Make sure this goes in site
-    perl Makefile.PL -n INSTALLDIRS=site
+    perl Makefile.PL INSTALLDIRS=site
     make
-    #make test # expected to fail as we don't had perl-heap-simple as dep to avoid cyclic dependency
+    make test
     make install
 else
     echo 'Unable to find Build.PL or Makefile.PL. You need to modify build.sh.'
